bea8d914d04b41232e7541bc9d356c24497f875b
TesterCC <testerlyx@foxmail.com>
2024-02-29 23:04:12 +0800

    update - use redis

--- controllers/player.go
+++ controllers/player.go
@@ -1,7 +1,6 @@
 package controllers
 
 import (
-	"gin_ranking/cache"
 	"gin_ranking/models"
 	"github.com/gin-gonic/gin"
 	"strconv"
@@ -25,14 +24,25 @@ func (p PlayerController) GetPlayers(c *gin.Context) {
 
 // GetRanking mysql orderby 排序实现排名展示
 func (p PlayerController) GetRanking(c *gin.Context) {
-	// https://www.imooc.com/video/24568  03:16
-	err := cache.Rdb.Set(cache.Rctx, "name", "zstest", 0).Err()
-	if err != nil {
-		panic(err)
-	}
+
+	//err := cache.Rdb.Set(cache.Rctx, "name", "zstest", 0).Err()
+	//if err != nil {
+	//	panic(err)
+	//}
 
 	aidStr := c.DefaultPostForm("aid", "0")
-	aid, _ := strconv.Atoi(aidStr)
+	aid, _ := strconv.Atoi(aidStr)   // activity id
+
+	// https://www.imooc.com/video/24568  5-2 05:30
+	// todo 新增逻辑：先从redis中获取数据，当redis中数据不存在时，再从mysql数据库中获取数据，且从mysql中获取数据后要存入redis中
+	// 使用redis时要注意设置过期时间
+
+	// method 1
+	//var redisKey string
+	//redisKey = "ranking:" + aidStr
+
+	// method 2 simplify
+	redisKey := "ranking:" + aidStr
 
 	rs, err := models.GetPlayers(aid, "score desc") // 按score字段降序排序
 	if err != nil {
